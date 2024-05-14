05029714a3454b09891d5208ea9c11d14c83734f
RubenVianna <viannaruben1@gmail.com>
2024-05-14 06:04:46 -0300

    cambios de estructura

---
+++ src/Models/nodo.py
@@ -0,0 +1,10 @@
+class Nodo:
+    def __init__(self, nombre):
+        self.nombre= nombre
+        self.coordenada_x= None
+        self.coordenada_y= None
+        self.heuristica= None
+        self.estado= None
+        self.conexiones= []
+
+    
