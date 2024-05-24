2861137983232fdab8a1a703dbc90ebacbcbc5e1
RubenVianna <viannaruben1@gmail.com>
2024-05-24 12:50:31 -0300

    se borro el int de los nodos

--- src/Controllers/herustica.py
+++ src/Controllers/herustica.py
@@ -1,12 +1,12 @@
 import math
 
 # Función para calcular la distancia en linea recta usando el teorema de pitagoras (distancia euclidiana) entre el nodo actual y el objetivo
-def distanciaLineaRecta(nodoAct: int, nodoFin: int):
+def distanciaLineaRecta(nodoAct, nodoFin):
     heuristica = math.sqrt((nodoFin.coordenada_x - nodoAct.coordenada_x) ** 2 + (nodoFin.coordenada_y - nodoAct.coordenada_y) ** 2)
     
     return math.trunc(heuristica) 
 # Función para calcular la distancia en linea recta entre 2 puntos usando la suma de la diferencia de las coordenadas entre el nodo actual y el objetivo
-def distanciaManhattan(nodoAct: int, nodoFin: int):
+def distanciaManhattan(nodoAct, nodoFin):
     herustica = abs(nodoFin.coordenada_x - nodoAct.coordenada_x) + abs(nodoFin.coordenada_y - nodoAct.coordenada_y)
     
     return math.trunc(herustica)
